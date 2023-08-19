250c62a8a6fffc06a2d316f1b18275e1baaa9595
Mpho Mabokela <mpmabok022@student.wethinkcode.co.za>
2023-08-19 13:53:53 +0200

    Calender API

--- SalesProjector.py
+++ SalesProjector.py
@@ -1,36 +1,54 @@
 import datetime
 import os
+import requests
 
+# def Variables():
+#     monthlyTarget = 20
+
+#     return monthlyTarget  
 
 def calculateDailyTarget():
+    
     os.system("clear")
-    #getting the monthly target from user
     monthlyTarget = 20
 
-    #for now our code can consit work on a 30 days calender
-    #over 4 weeks
-    weeklytarget = monthlyTarget/4
-    dailytarget = monthlyTarget/30
-
-    print("Your weekly target is: ",weeklytarget)
-    print("Your daily target is ",dailytarget)
-
-    #we are starting with a new target after a day
-    salesOnTheDay = 1
-    NewMonthlyTarget = monthlyTarget -salesOnTheDay
-    print("your new Traget is: ", NewMonthlyTarget)
+    api_key = 'MY API_KEY'
     
-    #calculating days left starting from 30 days and 5 days in a week
-    DaysInWeek = 7
-    salesprogress =1
-    dayscomplete = 1
+    response = requests.get(f'https://calendarific.com/api/v2/holidays?api_key={api_key}&country=US&year=2023&month=8')
+    data = response.json()
+
+    working_days = data['response']['holidays'][0]['date']['datetime']['day']
 
 
-    projectedMonthlyTarget = monthlyTarget - (salesprogress*dayscomplete)
+    #for now our code can consit work on a 30 days calendar
+    dailytarget = monthlyTarget/working_days
+    print("Your daily target is ",dailytarget)
+    
 
-    print("your new Projection ",projectedMonthlyTarget)
 
-# def calculate
+# def calculateWeeklytarget(monthlyTarget): 
+
+#     #calculates the over 4 weeks
+#     weeklytarget = monthlyTarget/4
+#     print("Your weekly target is: ",weeklytarget)
+
+
+#     #we are starting with a new target after a day
+#     salesOnTheDay = 1
+#     NewMonthlyTarget = monthlyTarget -salesOnTheDay
+#     print("your new Traget is: ", NewMonthlyTarget)
+    
+#     #calculating days left starting from 30 days and 5 days in a week
+#     DaysInWeek = 7
+#     salesprogress =1
+#     dayscomplete = 1
+
+
+#     projectedMonthlyTarget = monthlyTarget - (salesprogress*dayscomplete)
+
+#     print("your new Projection ",projectedMonthlyTarget)
+
+
 
 
 def main():
@@ -38,11 +56,14 @@ def main():
 
     while True:
         print("WELCOME TO SAFE PROJECTOR")
-        print("1. Calculate Daily Target")
+        print("1. See Daily Target")
+        print("2. See Weekly Target")
         print("2. Sales Forecast")
         print("3. Project Your progress success")
         option = input()
         if option == "1":
             calculateDailyTarget()
+        # if option == "2":   
+        #     calculateWeeklytarget(monthlyTarget)
 
 main()
